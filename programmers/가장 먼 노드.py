6d05b004cb2fae4b4d997e405e63f8d492eed22e
Jihye Lee <30456239+jihye1996@users.noreply.github.com>
2021-08-16 12:34:20 +0900

    Update 가장 먼 노드.py

--- programmers/가장 먼 노드.py
+++ programmers/가장 먼 노드.py
@@ -1,13 +1,14 @@
 def solution(n, edge):
     answer = 0
-    visited = [0 for i in range(n)]
+    visited = [0 for i in range(n)] # for is visit and distance
     graph = {i:[] for i in range(1, n+1)}
     
+    # make graph
     for x, y in edge:
         graph[x].append(y)
         graph[y].append(x)
     
-    
+    # bfs
     queue = [1]
     visited[0] = 0
     while len(queue) != 0:
