7b39c52c34cac041bb501bd3a5a48b9f33bfcfe2
Jihye Lee <30456239+jihye1996@users.noreply.github.com>
2021-08-16 12:33:49 +0900

    Create 가장 먼 노드.py

---
+++ programmers/가장 먼 노드.py
@@ -0,0 +1,20 @@
+def solution(n, edge):
+    answer = 0
+    visited = [0 for i in range(n)]
+    graph = {i:[] for i in range(1, n+1)}
+    
+    for x, y in edge:
+        graph[x].append(y)
+        graph[y].append(x)
+    
+    
+    queue = [1]
+    visited[0] = 0
+    while len(queue) != 0:
+        c = queue.pop(0)
+        for v in graph[c]:
+            if visited[v-1] == 0 and v != 1:
+                queue.append(v)
+                visited[v-1] = visited[c-1] + 1
+
+    return visited.count(max(visited))
