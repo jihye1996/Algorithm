1f201a4a418656584c16dc1c897336c67955554e
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-09-10 03:17:35 +0900

    Update 단속카메라.py

--- programmers/단속카메라.py
+++ programmers/단속카메라.py
@@ -1,14 +1,26 @@
 def solution(routes):
+    routes.sort(key=lambda x: x[1])
+    cur = -30001
+
+    answer = 0
+    for route in routes:
+        if cur < route[0]:
+            answer += 1
+            cur = route[1]
+    return answer
+'''
+def solution(routes):
     answer = []
     routes.sort()
-    #print(routes)
+
     for x, y in routes:
         if len(answer)==0:
             answer.append([x,y])
-        if x < answer[-1][1]:
+        if x <= answer[-1][1]:
             answer[-1][0] = max(answer[-1][0], x)
             answer[-1][1] = min(answer[-1][1], y)
         else:
             answer.append([x,y])
-    #print(answer)
+
     return len(answer)
+'''
