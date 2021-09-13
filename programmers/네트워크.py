5b50b47ae390b45ea7c87a4e5e90b06a8f4908e3
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-09-13 19:55:09 +0900

    Update 네트워크.py

--- programmers/네트워크.py
+++ programmers/네트워크.py
@@ -15,3 +15,24 @@ def solution(n, computers):
                     if computers[front][j] == 1 and visited[j] == False:
                         queue.append(j)
     return answer
+'''
+def solution(n, computers):
+    answer = 0
+    visit = [False] * n
+    queue = []
+    
+    for i in range(n):
+        if visit[i] == False:
+            answer += 1
+            queue.append(i)
+            
+            while 0 < len(queue):
+                front = queue.pop(0)
+                visit[front] = True
+                for j in range(n):
+                    if visit[j] == False and computers[front][j] == 1:
+                        queue.append(j)
+        #print(visit)
+               
+    return answer
+'''
