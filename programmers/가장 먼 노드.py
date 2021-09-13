a50b98baa3ac2e4c1885767b4a45f3bbfe5e73eb
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-09-13 20:49:43 +0900

    Update 가장 먼 노드.py

--- programmers/가장 먼 노드.py
+++ programmers/가장 먼 노드.py
@@ -19,3 +19,25 @@ def solution(n, edge):
                 visited[v-1] = visited[c-1] + 1
 
     return visited.count(max(visited))
+
+'''
+# retry 21.09.13
+def solution(n, edge):
+    answers = [0] * (n+1)
+    visit = [False] * (n+1)
+    nodes = {i: [] for i in range(1,n+1)}
+    queue = [1]
+    for x, y in edge:
+        nodes[x].append(y)
+        nodes[y].append(x)
+
+    while 0 < len(queue):
+        front = queue.pop(0)
+        visit[front] = True
+        for v in nodes[front]:
+            if visit[v] == False and v not in queue:
+                queue.append(v)
+                answers[v] = answers[front] + 1
+    max_value = max(answers)
+    return answers.count(max_value)
+'''
