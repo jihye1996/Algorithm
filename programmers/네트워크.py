1e7494f726c6332b721101fedabe466e17fbc2ae
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-09-13 21:28:51 +0900

    Update 네트워크.py

--- programmers/네트워크.py
+++ programmers/네트워크.py
@@ -16,6 +16,7 @@ def solution(n, computers):
                         queue.append(j)
     return answer
 '''
+retry 21.09.13 dfs with queue
 def solution(n, computers):
     answer = 0
     visit = [False] * n
@@ -34,5 +35,42 @@ def solution(n, computers):
                         queue.append(j)
         #print(visit)
                
+    return answer
+ 
+retry 21.09.13 dfs with stack
+def solution(n, computers):
+    answer = 0
+    visit = [False] * n
+    stack = []
+    for i in range(n):
+        if visit[i] == False:
+            stack.append(i)
+            answer += 1
+            
+            while 0 < len(stack):
+                top = stack.pop()
+                visit[top] = True
+                for j in range(n):
+                    if computers[top][j] == 1 and visit[j] == False:
+                        stack.append(j)
+    return answer
+    
+retry 21.09.13 dfs with recursive
+def dfs(root, visit, computers):
+    
+    visit[root] = True
+    for j in range(len(visit)):
+        if computers[root][j] and not visit[j]:
+            dfs(j, visit, computers)
+
+def solution(n, computers):
+    answer = 0
+    visit = [False] * n
+    
+    for i in range(n):
+        if visit[i] == False:
+            answer += 1
+            dfs(i, visit, computers)
+            
     return answer
 '''
