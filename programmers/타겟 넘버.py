97f6e761eb7668433a25299b8f84dbfb1f08534c
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-09-14 16:51:08 +0900

    Update 타겟 넘버.py

--- programmers/타겟 넘버.py
+++ programmers/타겟 넘버.py
@@ -3,3 +3,21 @@ def solution(numbers, target):
     data = [(i, -i) for i in numbers]
     result = list(map(sum, product(*data)))
     return result.count(target)
+
+'''
+retry 21.09.14
+def solution(numbers, target):
+    length = len(numbers)
+    num_list = [[numbers[0], 0], [-1*numbers[0], 0]]
+    answer = 0
+    while 0 < len(num_list):
+        v, i = num_list.pop()
+        if i == length-1:
+            if target == v:
+                answer += 1
+        else:
+            num_list.append([v+numbers[i+1],i+1])
+            num_list.append([v-numbers[i+1],i+1])
+            
+    return answer
+'''
