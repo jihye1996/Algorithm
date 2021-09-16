8ec4f994969c853c788b5af79858a957c546b47f
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-09-16 10:15:26 +0900

    Update 삼각 달팽이.py

--- programmers/삼각 달팽이.py
+++ programmers/삼각 달팽이.py
@@ -1,4 +1,17 @@
 def solution(n):
-    answer = []
-    # 삼각형에 숫자를 1씩 증가하게 값 입력하는 문제
-    return answer
+    answer = [[0 for j in range(1, i+1)] for i in range(1, n+1)] #2차원배열
+    
+    num = 1
+    x, y = -1, 0
+    for step in range(n):
+        for j in range(n-step):
+            if step % 3 == 0: #하
+                x+=1
+            elif step % 3 == 1:#우
+                y+=1
+            else:#상
+                x-=1
+                y-=1
+            answer[x][y] = num
+            num +=1       
+    return sum(answer, []) #2차원배열 -> 1차원배열
