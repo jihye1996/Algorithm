4029e6cf7637e4885ee058fad0dc1f46016fe4a1
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-09-27 09:44:10 +0900

    Update 도둑질.py

--- programmers/도둑질.py
+++ programmers/도둑질.py
@@ -1,14 +1,14 @@
 def solution(money):
-    dp = [0 for i in range(len(money))]
+    dp1 = [0 for i in range(len(money))]
+    dp2 = [0 for i in range(len(money))]
+
+    # 1번 집을 터는 경우 
+    dp1[0] = money[0] 
+    for i in range(1, len(money) - 1): 
+        dp1[i] = max(dp1[i - 1], dp1[i - 2] + money[i]) 
     
-    for i in range(len(money)):
-        dp[i] = odd += money[2*i]
-        even += money[2*i+1]
-
-    answer = max(odd, even)
-
-    if len(money) % 2 == 1:
-        answer  = max(answer, odd - money[0] + money[-1])
-
-    print(answer)
-    return answer
+    # 1번 집을 안터는 경우  
+    for i in range(1, len(money)): 
+        dp2[i] = max(dp2[i - 1], dp2[i - 2] + money[i])
+    
+    return max(dp1[-2], dp2[-1])
