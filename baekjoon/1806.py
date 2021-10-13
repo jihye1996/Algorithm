4ebc203d0675152fc565fb9fe158a22c318fe889
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-10-13 09:42:23 +0900

    Update 1806.py

--- baekjoon/1806.py
+++ baekjoon/1806.py
@@ -1,26 +1,19 @@
-# 21.10.12: 수정 필요 
-# 10 0, 1 1 1 1 1 1 1 1 1 1 케이스 틀림
 N, S = map(int, input().split())
 N_list = list(map(int, input().split()))
 
-left, right, total = 0, 0, N_list[0]
+left, right, total = 0, 0, 0
 min_len = 100001
-index = 1
-while left < N-1: 
+while True: 
     if S <= total:
-        min_len = min(min_len, right - left + 1)
+        min_len = min(min_len, right - left)
         total -= N_list[left]
         left += 1
     else:
-        if right < N - 1:
-            right += 1
-            total += N_list[right]
+        if right == N:
+            break
         else:
-            total -= N_list[left]
-            left += 1
-
-    #print(index, ': ', left, ' ', right, ' ', total, ' ', min_len)
-    index+= 1
+            total += N_list[right]
+            right += 1
 if min_len == 100001:
     print(0)
 else:
