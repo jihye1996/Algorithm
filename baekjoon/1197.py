b094a7340b974dbfd291626199fbf59953f0b688
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-10-25 10:11:34 +0900

    Update 1197.py

--- baekjoon/1197.py
+++ baekjoon/1197.py
@@ -1,28 +1,30 @@
-# 2021.10.20
-
 import sys
 import heapq
 
 input = sys.stdin.readline
-answer = 0
+answer, cnt = 0, 0
 V, E = map(int, input().split())
-graph = []
-visited = [False] * E
+graph = [[] for _ in range(V+1)]
+visited = [False] * (V+1)
+heap = [[0, 1]] # 비용, V
 
 for _ in range(E):
     v1, v2, g = map(int, input().split())
-    heapq.heappush(graph, (g, v1, v2))
+    graph[v1].append([g, v2])
+    graph[v2].append([g, v1])
 
-connected = 0
-# 수정 필요 
-while graph or connected < V - 1:
+while heap:
+    if cnt == V:
+        break
     
-    g, v1, v2 = heapq.heappop(graph)
-    if visited[v1-1] and visited[v2-1]:
-        continue
-    else:
-        visited[v1-1], visited[v2-1] = True, True
+    g, v = heapq.heappop(heap)
+    if not visited[v]:
+        visited[v] = True
         answer += g
-        connected += 1
+        cnt += 1
+        
+        for t in graph[v]:
+            heapq.heappush(heap, t)
+        
 
 print(answer)
