7c1d9135913ad78299ced9778bbdb3fb352beff5
Jihye <30456239+jihye1996@users.noreply.github.com>
2021-11-10 10:47:07 +0900

    Update 1038.java

--- baekjoon/1038.java
+++ baekjoon/1038.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
 public class Main{
+    static ArrayList<Long> array = new ArrayList<>();
 
-     public static void main(String []args){
+    public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         sc.close();
@@ -12,14 +13,22 @@ public class Main{
         }else if(N>1022){
             System.out.println("-1");
         }else{
-            ArrayList<Integer> array = new ArrayList<>();
             
+            for(long i=0; i<10; i++){
+                descending_number(i);
+            }
             Collections.sort(array);
             System.out.println(array.get(N));
+
         }
     }
     
-    private static void descedning_number(){
+    private static void descending_number(long num){
+        //if(depth>10) return;
+        array.add(num);
         
+        for(int j=0; j<num%10; j++){
+            descending_number(num*10+j);
+        }
     }  
 }
